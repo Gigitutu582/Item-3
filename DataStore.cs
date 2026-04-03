14e3007e131931277be5b6c93eca5e5089509c4d
Gigitutu582 <d84532297@gmail.com>
2026-04-03 15:28:34 +0300

    Update DataStore.cs

--- DataStore.cs
+++ DataStore.cs
@@ -7,5 +7,5 @@ public static class DataStore
     public static List<string> Participants { get; } = new();
     public static ConcurrentDictionary<string, string> Assignments { get; } = new();
     public static ConcurrentDictionary<string, string> Wishes { get; } = new();
-    public static object Lock { get; } = new object();   
+    public static readonly object Lock = new object();
 }
